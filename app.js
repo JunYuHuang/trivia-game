bfc14735e6610ee0b1d8817d6020f3f9ca93a814
Jun Huang <junyu_huang@hotmail.com>
2019-11-02 00:06:30 -0700

    implemented basic functionality

--- app.js
+++ app.js
@@ -1,25 +1,39 @@
 // query selectors and global variables
-const app = document.querySelector("#app");
-const questionsContainer = document.querySelector("#questions-container");
+const questionContainer = document.querySelector("#question-container");
 const answersContainer = document.querySelector("#answers-container");
-const radioButtonAnswers = document.querySelector("#answers-container input");
+const buttonNextQuestion = document.querySelector("#button-next-question");
+let question = "";
+let correctAnswer = "";
+let incorrectAnswers = [];
+let answers = [];
 
-// load up initial HTML template
-app.innerHTML = `
-  <div class="row">
-    <h5 id="brand-text">🎮 Video Game Trivia</h5>
-  </div>
-  <div class="row" id="question-container"></div>
-  <div class="row" id="answer-container"></div>
-`;
+buttonNextQuestion.addEventListener("click", () => {
+  startNewGame();
+});
 
 // start initial game
+startNewGame();
 
-// function for generating 1 trivia set
-// get API data, create UI elements that display q+a, reset values
+function startNewGame() {
+  resetTriviaSetUI();
+  getAPIData()
+    .then(data => {
+      question = data.question;
+      correctAnswer = data.correct_answer;
+      incorrectAnswers = data.incorrect_answers;
+      answers = shuffle([...incorrectAnswers, correctAnswer]);
 
-// get API data
-async function getAPIData(question_count, question_category, question_type) {
+      generateTriviaSetUI(question, answers);
+    })
+    .catch(error => console.log(error));
+}
+
+// get API data for 1 trivia set (1 question + 1 correct answer + 3 incorrect answers)
+async function getAPIData(
+  question_count = 1,
+  question_category = 15,
+  question_type = "multiple"
+) {
   const apiResponse = await fetch(
     `https://opentdb.com/api.php?amount=${question_count}&category=${question_category}&type=${question_type}`
   );
@@ -27,14 +41,69 @@ async function getAPIData(question_count, question_category, question_type) {
   return data.results[0];
 }
 
-// create input element and event handler for each answer
+// display 1 trivia set
+function generateTriviaSetUI(question, answers) {
+  questionContainer.insertAdjacentHTML("beforeend", `<h4>${question}</h4>`);
 
-// display trivia question and answer inputs in UI
+  answers.forEach(answer => {
+    answersContainer.insertAdjacentHTML(
+      "beforeend",
+      `<div class="row answer-item"> ${answer}</div>`
+    );
+  });
+
+  const answerItems = document.querySelectorAll(
+    "#answers-container div.answer-item"
+  );
+
+  // respond in UI depending on which answer was clicked
+  answersContainer.addEventListener("click", event => {
+    // any of the "answer" divs must be clicked to trigger response
+    if (event.target.matches("div.answer-item")) {
+      // evaluate each answer
+      answerItems.forEach(item => {
+        // dim all answers that were not clicked on by the user
+        if (item !== event.target) {
+          item.classList.add("answer-item-not-selected");
+        }
+        // set the correct answer's background to green and prepend it with a checkmark
+        if (item.innerText === correctAnswer) {
+          item.classList.add("answer-item-correct");
+          item.insertAdjacentHTML("afterbegin", "&#x2714; ");
+        } else {
+          // set all incorrect answers' background to red and prepend them each with a cross
+          item.classList.add("answer-item-incorrect");
+          item.insertAdjacentHTML("afterbegin", "&#x2718; ");
+        }
+      });
+
+      // disable all answer options once any answer is clicked
+      answerItems.forEach(item => {
+        item.classList.add("disabledElement");
+      });
+    }
+  });
+}
+
+// reset app ui; delete all child elements under the question and answers divs
+function resetTriviaSetUI() {
+  const childOfQuestionContainer = Array.prototype.slice.call(
+    questionContainer.getElementsByTagName("*")
+  );
+  const childrenOfAnswersContainer = Array.prototype.slice.call(
+    answersContainer.getElementsByTagName("*")
+  );
+  childOfQuestionContainer.forEach(childElement => childElement.remove());
+  childrenOfAnswersContainer.forEach(childElement => childElement.remove());
+}
 
 // shuffle order of answers
+function shuffle(array) {
+  var clone = array.slice(0);
+  for (let i = clone.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [clone[i], clone[j]] = [clone[j], clone[i]];
+  }
 
-// enable all answer inputs
-
-// disable all answer inputs
-
-// display results
+  return clone;
+}
